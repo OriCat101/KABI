42bd074265ef64c49e3b952ef5c48eb0ac0c96e2
n0_username <redbadcommander@protonmail.ch>
2021-03-05 16:29:19 +0100

    Prepared to connect the server to the client.

--- server.c
+++ server.c
@@ -6,3 +6,32 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+
+
+int main() {
+
+  int sock. client_socket;
+  char buffer[1024];
+  char response[18384];
+  struct sockaddr_in, server_address, client_address;
+  int i=0;
+  int optval = 1;
+  socklen_t client_length;
+
+  sock = socket(AF_INET, SOCK_STREAM,  0);
+
+  if (setsockopt(sock, SOL_SOCKET, SOL_REUSEADDR, &optval, sizeof(optval)) < 0) {
+      printf("Error Setting TCP Socket Options!\n");
+      return 1;
+  }
+
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = inet_addr("192.168.1.224");
+  server_address.sin_port = htons(3141);
+
+  bind(sock, (struct sockaddr *) &server_address, sizeof(server_address));
+  listen(sock, 5);
+  client_length = sizeof(client_address);
+  client_socket = accept(sock, (struct sockaddr *) &client_address, &client_length);
+
+}
