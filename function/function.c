e4d3e8952d6b4309d8af65a9fdfb959f0c06e8bf
WeedFox <redbadcommander@protonmail.ch>
2021-03-07 21:30:40 +0100

    Began to recreate the code from the function.png

--- function/function.c
+++ function/function.c
@@ -4,5 +4,18 @@ str_cut(char str[], int slice_from, int slice_to){
     return NULL;
 
   char *buffer;
+  size_t str_len, buffer_len;
+
+  if (slice_to < 0 && slice_from > slice_to) {
+    str_len = strlen(str);
+    if (abs(slize_to) > str_len - 1)
+      return NULL;
+
+    if (abs(slize_from) > strlen)
+      slice_from = (-1) * str_len;
+
+    buffer_len = slice_to - slice_from;
+
+  }
 
 }
